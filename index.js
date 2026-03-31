206c02f29a76fd02d66d3a4bda237f36faaf6b6f
LuanaSantos32 <tiprosub.luana@gmail.com>
2026-03-30 21:03:55 -0300

    finalizando a atividade

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function mudar_conteudo_elemento() {
+function mudar_conteudo_elemento(){
 
-document.getElementById("h1").innerHTML="Hello JavaScript!";
+  document.getElementById("h1").innerHTML = "Hello Javascript!";
+
+}
+function mudar_conteudo_paragrafo(){
+
+  document.getElementById("p").innerHTML = "minha função de mudar o conteúdo funcionou!";
+
 }
