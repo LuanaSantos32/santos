f1e8a7bc83850a4550cfae5af661077e1ac85585
LuanaSantos32 <tiprosub.luana@gmail.com>
2026-04-13 20:40:32 -0300

    alteraçao do o alinhamento do texto para a direita .

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function mudar_estilo(){
 document.getElementById("paragrafo").style.textAlign = "right";
 }
 
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
